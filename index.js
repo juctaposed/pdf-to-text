2c9459c553cfafd0740f4d75180fb549cf46763c
Juctaposed <juctaposed@gmail.com>
2023-02-11 13:27:30 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const pdfParse = require('pdf-parse')
+
+const app = express()
+
+app.use('/', express.static('public'))
+app.use(fileUpload())
+
+
+app.post('/extract-text', (req, res) => {
+    if(!req.files && !req.files.pdfFile) {
+        res.statusCode(400)
+        res.end()
+    }
+
+    pdfParse(req.files.pdfFile).then(results => {
+        res.send(results.text)
+    })
+})
+
+app.listen(3000)
